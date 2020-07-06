9710fa6a6b35a1c26ece6e5996c1f06aa31fbfcd
Christian Czymara <czymara@gmail.com>
2020-07-06 12:21:41 +0200

    initial

---
+++ EvalOverTime.R
@@ -0,0 +1,85 @@
+
+
+### plot time trend of course evaluations
+
+evals <- data.frame("semester" = c("2016", "2016/17", "2017", "2017/18",
+                                   # frankfurt
+                                   "2018", "2018/19",
+                                   "2019", "2019/20",
+                                   "2020"),
+                    "mean" = c(2, 1.6, NA, 1.4,
+                               # frankfurt
+                               1.7,
+                               1.6, 1.4,
+                               1.5, 1.4
+                               ),
+                    "sd" = c(0, 0.7, NA, 0.6,
+                             # frankfurt
+                             0.5,
+                             0.5, 0.5,
+                             0.7, 1.2
+                             ),
+                    "n" = c(5, 21, NA, 18,
+                            # frankfurt
+                            6,
+                            10, 8,
+                            12, 19
+                            ),
+                    "University" = c("CGN", "CGN", "CGN", "CGN",
+                              "FRA", "FRA", "FRA", "FRA", "FRA"),
+                    "worth" = c (NA, NA, NA, NA, NA,
+                                 5.8, 5.3, 5.5, 5.9),
+                    "worthSD" = c (NA, NA, NA, NA, NA,
+                                 0.4, 0.8, 0.7, 0.3)
+                    )
+
+# for inference statistics
+# evals$CIupper <- evals$mean + 1.96*(evals$sd/sqrt(evals$n))
+# evals$CIlower <- evals$mean - 1.96*(evals$sd/sqrt(evals$n))
+
+evals$grandmean <- mean(evals$mean, na.rm = T)
+
+evals$worthgrandmean <- mean(evals$worth, na.rm = T)
+
+library(ggplot2); theme_set(theme_bw() +
+                              theme(axis.line = element_line(colour = "black"),
+                                    panel.grid.major = element_blank(),
+                                    panel.grid.minor = element_blank(),
+                                    panel.border = element_blank(),
+                                    panel.background = element_blank()))
+
+# win.metafile("C:/Users/czymara.local/PowerFolders/teaching/out/evalovertime.wmf")
+dev.copy(png, "C:/Users/czymara.local/PowerFolders/teaching/out/evalovertime.png",
+         units="px", width=1600, height=1600, res=300)
+ggplot(data = evals, aes(y = semester,
+                         x = mean,
+                         colour = University)) +
+  geom_errorbarh(aes(xmin = mean - sd,
+                     xmax = mean + sd),
+                 colour = "grey") +
+  xlab("Overall grade (1: best)") + ylab("") +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title = "Teaching evaluation of C. Czymara",
+       caption = "Means and standard deviations
+       Overall grade (left y-axis): Until 2018 scale 1-5,
+       from 2018/19 scale 1-6") +
+ # theme(legend.position = "none") +
+  # scale_x_continuous(limits = c(0, 6.5), breaks = c(1:6)) +
+  geom_path(aes(group = 1), colour = "grey") +
+  geom_vline(xintercept = evals$grandmean ,
+             colour = "black") +
+  geom_point() +
+  geom_errorbarh(data = evals, aes(xmin = worth - worthSD,
+                     xmax = worth + worthSD),
+                 colour = "grey") +
+  scale_x_continuous(limits = c(0, 6.5), breaks = c(1:6),
+                     sec.axis = sec_axis(~. , breaks = c(1:6),
+                                         name = "Course worth\nattending (6: agree)")
+                     ) +
+  geom_path(data = evals, aes(x = worth, group = 1), colour = "grey") +  # add the second axis
+  geom_point(data = evals, aes(y = semester,
+                               x = worth,
+                               colour = University)) +
+coord_flip()
+dev.off()
+
---
+++ evalationwordclouds.R
@@ -0,0 +1,75 @@
+
+
+### create worldcloude of open evaluation items
+
+evaluations <- read.delim("C:/Users/czymara.local/PowerFolders/teaching/teachingevaluations/in/evaluations.txt", fileEncoding="UTF-8")
+
+evaluations <- as.character(evaluations[,1])
+
+library("quanteda", "tidytext", "dplyr")
+
+
+toks <- tokens(corpus(evaluations), remove_punct = T,
+               remove_numbers = T,
+               remove_symbols = T,
+               remove_separators = T,
+               remove_hyphens = T)
+
+toks <-  tokens_remove(toks, c(stopwords("german"),
+                               "dass", "h", "w?re", "w?ren",
+                               "bzw", "pro", "eher"), case_insensitive = TRUE, padding = FALSE)
+toks <-  tokens_remove(toks, stopwords(), case_insensitive = TRUE, padding = FALSE)
+
+DFM <- dfm(toks)
+
+
+# wordcloud
+
+win.metafile("C:/Users/czymara.local/PowerFolders/teaching/out/eval_wordcloud.wmf")
+# dev.copy(png,"C:/Users/czymara.local/PowerFolders/teaching/out/eval_wordcloud.png")
+textplot_wordcloud(DFM,
+                  # min_size = 1.5,
+                 #  min_count = 3,
+                   color = "black")
+dev.off()
+
+
+
+## sentiment analysis
+
+# create dictionary (see https://www.inwt-statistics.de/blog-artikel-lesen/text-mining-part-3-sentiment-analyse.html)
+SentiWS <- c(
+  readLines("C:/Users/czymara.local/Google Drive/job/zonstiges/spielRei/twitter/SentiWS_v2.0_Positive.txt",
+            encoding = "UTF-8"),
+  readLines("C:/Users/czymara.local/Google Drive/job/zonstiges/spielRei/twitter/SentiWS_v2.0_Negative.txt",
+            encoding = "UTF-8")
+) %>% lapply(function(x) {
+  # Extrahieren der einzelnen Spalten
+  res <- strsplit(x, "\t", fixed = TRUE)[[1]]
+  return(data.frame(word = res[1], value = res[2],
+                    stringsAsFactors = FALSE))
+}) %>%
+  bind_rows %>%
+  mutate(word = gsub("\\|.*", "", word) %>% tolower,
+         value = as.numeric(value)) %>%
+  # manche Wörter kommen doppelt vor, hier nehmen wir den mittleren Wert
+  group_by(word) %>% summarise(value = mean(value)) %>% ungroup
+
+
+
+DFM %>%
+  inner_join(SentiWS) %>%
+  count(word, sort = TRUE)
+
+words <- NULL
+words$word <- colnames(DFM)
+
+# mean sentiment
+sentTwt <- left_join(words,
+                     SentiWS,
+                     by = "word") %>%
+  mutate(value = as.numeric(value)) %>%
+  filter(!is.na(value))
+
+mean(sentTwt$value) # negative
+
